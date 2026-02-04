5975c5c8e8e84545ee990f0c816834ca61e2ab7f
Sara Krishevsky <sarilimud@gmail.com>
2026-02-05 01:53:40 +0200

    Add music service with API calls

---
+++ src/app/services/music.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin, of } from 'rxjs';
+import { map, catchError, switchMap } from 'rxjs/operators';
+
+export interface Song {
+	id: string;
+	title: string;
+	artist: string;
+	album?: string;
+	artworkUrl?: string | null;
+	description?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class MusicService {
+	private readonly MB_SEARCH_URL = 'https://musicbrainz.org/ws/2/recording';
+	private readonly COVER_ART_URL = 'https://coverartarchive.org/release';
+
+	constructor(private http: HttpClient) {}
+
+  searchSongs(query: string, limit: number = 20): Observable<Song[]> {
+    const params = {
+      query: query,
+      fmt: 'json',
+      limit: String(limit)
+    };
+
+    return this.http.get<any>(this.MB_SEARCH_URL, { params }).pipe(
+      map(resp => (resp.recordings || []).map((rec: any) => {
+        const artistCredit = rec['artist-credit']?.[0] || {};
+        const release = rec.releases?.[0] || null;
+
+        return {
+          id: rec.id,
+          title: rec.title,
+          artist: artistCredit.name || artistCredit.artist?.name || 'Unknown',
+          album: release?.title,
+          artworkUrl: release ? `${this.COVER_ART_URL}/${release.id}` : null,
+          description: rec.disambiguation || release?.disambiguation || ''
+        } as Song;
+      })),
+      catchError(() => of([]))
+    );
+  }
+}
