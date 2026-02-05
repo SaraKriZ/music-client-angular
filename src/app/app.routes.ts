d3a3aeed86e0ed66de18c16d57ed8dae2bc4f5a4
Sara Krishevsky <sarilimud@gmail.com>
2026-02-05 02:33:47 +0200

    Route to music list and music details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { MusicListComponent } from './features/music-list/music-list.component';
 import { MusicDetailsComponent } from './features/music-details/music-details.component';
 
 export const routes: Routes = [
-	{ path: '', component: MusicListComponent },
-	{ path: 'detail/:id', component: MusicDetailsComponent }
+	{ path: '', redirectTo: 'music', pathMatch: 'full' },
+	{ path: 'music', component: MusicListComponent },
+	{ path: 'music/:id', component: MusicDetailsComponent },
+	{ path: '**', redirectTo: 'music' }
 ];
